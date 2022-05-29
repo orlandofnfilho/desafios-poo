b7dcfe0ac5aa6a3279270d130418e09f0a34d982
Orlando Ferreira <75701257+orlandofnfilho@users.noreply.github.com>
2022-05-29 20:37:54 -0300

    update

---
+++ test/br/com/gft/desafio01/model/VeiculoTest.java
@@ -0,0 +1,14 @@
+package br.com.gft.desafio01.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VeiculoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
